34bb8924ab63b471a0a82d466a19e7e6a88955fe
JaydeepDesai <Jaydeep.Desai@programmers.io>
2023-07-24 00:47:57 +0530

    Dashboard view

---
+++ src/main/resources/js/script.js
@@ -0,0 +1,12 @@
+const toggleSidebar = () => {
+    if($(".sidebar").is(":visible")){
+    //hide
+        $(".sidebar").css("display", "none");
+        $(".content").css("margin-left", "0%");
+    }
+   else{
+   //show
+        $(".sidebar").css("display", "block");
+        $(".content").css("margin-left", "20%");
+   }
+}
